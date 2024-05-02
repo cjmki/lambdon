4affc4878adf5e58aa4e83332dcc488bdf37bd0d
Charith Jayawardana <cvljayawardana@gmail.com>
2024-05-02 21:35:03 +0200

    feat: add clap & rataui

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use owo_colors::OwoColorize;
+use clap::Parser;
 
 const OWO: &str = r#"
 ██╗      █████╗ ███╗   ███╗██████╗  ██████╗ ███╗   ██╗
@@ -10,7 +11,23 @@ const OWO: &str = r#"
                                                           
 "#;
 
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    name: String,
+
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
 fn main() {
-    println!("\n\n\n\n\n{}", OWO.fg_rgb::<0x2E, 0x31, 0x92>().bold());
+    println!("\n\n\n\n\n{}", OWO.fg_rgb::<0x00, 0xFF, 0x00>().bold());
     println!("\n\n\n\n\n\n");
+
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name)
+    }
 }
